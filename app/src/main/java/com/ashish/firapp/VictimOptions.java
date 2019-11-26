accfd10576836981cbb8f70496bdfbcc70bcff95
Ashish <ashish.chauhan_mca18@gla.ac.in>
2019-11-27 01:33:18 +0530

    5 MORE ACTIVITES ARE ADDED

---
+++ app/src/main/java/com/ashish/firapp/VictimOptions.java
@@ -0,0 +1,14 @@
+package com.ashish.firapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class VictimOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_victim_options);
+    }
+}
---
+++ app/src/main/java/com/ashish/firapp/VictimTable.java
@@ -0,0 +1,13 @@
+package com.ashish.firapp;
+
+public class VictimTable {
+    String name,mob,email,username,password;
+    public VictimTable(String A,String B,String C, String D, String E)
+    {
+        this.name=A;
+        this.mob=B;
+        this.email=C;
+        this.username=D;
+        this.password=E;
+    }
+}
